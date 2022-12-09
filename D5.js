83e1c20b96a31123aaf4a2ae1300c53bfb72d6d3
Palma Iacobelli <palmaiacobelli92@gmail.com>
2022-12-09 13:44:33 +0100

    Update D5.js

--- D5.js
+++ D5.js
@@ -99,13 +99,27 @@ console.log(justTrims);
     Cicla l'array "cars" e costruisci un if/else statament per mostrare due diversi messaggi in console. Se la prima lettera della proprietà
     "color" ha valore "b", mostra in console "Fizz". Altrimenti, mostra in console "Buzz".
 */
-
+for (let i = 0; i < cars.length; i++) {
+  if (cars[i].color.split("") === "b") {
+    console.log("Fizz");
+  } else {
+    if (cars[i].color.split("") !== "b") {
+      console.log("Buzz");
+    }
+  }
+}
 /* ESERCIZIO 9
     Utilizza un ciclo while per stampare in console i valori del seguente array numerico fino al raggiungimento del numero 32.
 */
 const numericArray = [
   6, 90, 45, 75, 84, 98, 35, 74, 31, 2, 8, 23, 100, 32, 66, 313, 321, 105,
 ];
+let i = 0;
+while (numericArray[i] === 32) {
+  console.log(numericArray[i]);
+  i++;
+}
+console.log(numericArray);
 
 /* ESERCIZIO 10
     Partendo dall'array fornito e utilizzando un costrutto switch, genera un nuovo array composto dalle posizioni di ogni carattere all'interno
@@ -113,3 +127,16 @@ const numericArray = [
     es. [f, b, e] --> [6, 2, 5]
 */
 const charactersArray = ["g", "n", "u", "z", "d"];
+
+const posizioniCaratteri = [];
+for (let i = 0; (i = 21); i++) {
+  posizioniCaratteri.push();
+}
+console.log(posizioniCaratteri);
+
+/* }
+for (let i = 0; i < charactersArray.length; i++) {
+  switch (true) {
+    case charactersArray[i]:
+  }
+}  */
